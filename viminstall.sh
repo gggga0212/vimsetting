3652006369fed44866bd16b9e6448695428fd0e2
SamKuo <30749497+gggga0212@users.noreply.github.com>
2022-03-22 21:44:47 +0800

    Add files via upload

---
+++ viminstall.sh
@@ -0,0 +1,26 @@
+sudo apt-get install git  
+sudo apt-get install curl  
+sudo apt-get install exuberant-ctags  
+sudo apt-get install cscope  
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gggga0212/vimsetting.git  
+cd vimsetting 
+cp vimrc ~/.vimrc
+
+#fzf
+#cd ~/.vim/bundle/fzf
+#./install
+
+#powerline-fonts
+#cd ~/.vim/bundle/powerline-fonts
+#./install.sh
+
+#>>>>Show colors in terminal.
+#vim ~/.bashrc
+#force_color_prompt=yes
+
+#>>>Show dir colors.
+#wget https://raw.github.com/trapd00r/LS_COLORS/master/LS_COLORS -O ~/.dircolors
+#vim ~/.dircolors
+

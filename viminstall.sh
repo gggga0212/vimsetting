8fecff5a5677773874ccba18683fd5654928473d
Sam Kuo <gggga0212@gmail.com>
2022-04-17 15:42:37 +0800

    update

--- viminstall.sh
+++ viminstall.sh
@@ -24,22 +24,25 @@ sudo apt-get install python-dev python-pip python3-dev python3-pip
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone https://github.com/gmarik/vundle.git ~/.config/nvim/bundle/vundle
 git clone https://github.com/gggga0212/vimsetting.git
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-pip3 install --user neovim
+#git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+#pip3 install --user neovim
+
 cd vimsetting
 cp vimrc ~/.vimrc
 cp vimrc ~/.config/nvim/init.vim
 cp gitconfig ~/.gitconfig
 cp tmux.conf ~/.tmux.conf
 
-cd ~/.config/nvim
-#mkdir lua
-#cp plugins.lua ~/.config/nvim/lua/plugins.lua
-
 #coc.vim
 #cd ~/.config/nvim/bundle/coc.nvim
 #npm install
 #:CocInstall coc-json coc-tsserver
+#:CocInstall coc-marketplace
+#:CocList marketplace
+
+#packer
+#cp plugins.lua ~/.config/nvim/lua/plugins.lua
+
 
 #fzf
 #cd ~/.vim/bundle/fzf

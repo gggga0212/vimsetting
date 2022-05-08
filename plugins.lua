0877af312f1f59ab577b5e94385c1807429d9a9f
SamKuo <30749497+gggga0212@users.noreply.github.com>
2022-05-08 15:34:23 +0800

    Delete plugins.lua

--- plugins.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd ([[packadd packer.nvim]])
-return require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
-    use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
-    use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim'}
-end)

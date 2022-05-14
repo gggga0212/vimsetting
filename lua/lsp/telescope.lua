8b1c9ef733be86aa6171989230621d8cc870f270
SamKuo <30749497+gggga0212@users.noreply.github.com>
2022-05-15 00:20:28 +0800

    Add files via upload

---
+++ lua/lsp/telescope.lua
@@ -0,0 +1,11 @@
+require('telescope').setup({
+    defaults = {
+        layout_config = {
+            -- vertical = { width = 0.9 }
+            width = 0.95 
+            -- other layout configuration here
+        },
+        -- other defaults configuration here
+    },
+    -- other configuration values here
+})

22648e49ff2bb2bd321f16271d24848b4d7ffc27
Sam.Kuo <Sam.Kuo@microchip.com>
2022-06-30 12:41:03 +0800

    lua

---
+++ lua/lsp/color-picker.lua
@@ -0,0 +1,11 @@
+local opts = { noremap = true, silent = true }
+
+
+require("color-picker").setup({ -- for changing icons & mappings
+    ["icons"] = { "ﱢ", "" },
+    ["keymap"] = { -- mapping example:
+    ["U"] = "<Plug>Slider5Decrease",
+    ["O"] = "<Plug>Slider5Increase",
+    },
+})
+vim.cmd([[hi FloatBorder guibg=NONE]]) 

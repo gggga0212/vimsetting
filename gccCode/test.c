7a35e0109195ce8ec5e4b245d1977cdca5c3d9de
Sam.Kuo <Sam.Kuo@microchip.com>
2022-07-24 20:47:48 +0800

    lua

--- gccCode/test.c
+++ gccCode/test.c
@@ -3,18 +3,18 @@
 int main(void)
 {
     int i;
-    FILE *fp;
-    fp = fopen("test.txt", "w+");
+    /* FILE *fp; */
+    /* fp = fopen("test.txt", "w+"); */
     /* fprintf(fp,"hexValue = ["); */
-    for(i=0;i<=0xFF;i++)
-    {
+    /* for(i=0;i<=0xFF;i++) */
+    /* { */
     /* fprintf(fp,"'"); */
-    fprintf(fp,"%c",i);
+    /* fprintf(fp,"%c",i); */
     /* fprintf(fp,"'"); */
     /* fprintf(fp,","); */
-    }
+    /* } */
     /* fprintf(fp,"]\n"); */
-    /* printf("Hello World.\n"); */
-    fclose(fp);
+    printf("Hello World.\n");
+    /* fclose(fp); */
     return 0;
 }

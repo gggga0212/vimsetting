0410fa0c182aef17833a9f1ca9ed770348b56856
Sam.Kuo <Sam.Kuo@microchip.com>
2022-07-31 23:40:21 +0800

    lua

--- lua/lsp/nvim-cmp.lua
+++ lua/lsp/nvim-cmp.lua
@@ -18,9 +18,9 @@
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      -- ['<C-c>'] = cmp.mapping.complete(),
+      -- ['<C-Space>'] = cmp.mapping.complete(),
       -- ['<C-e>'] = cmp.mapping.abort(),
-      ['<C-e>'] = cmp.mapping.confirm({ select = true }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
       ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()

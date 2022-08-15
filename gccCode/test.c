c459feb97f0bf7687cfd30d37acf1ee06facf4f6
Sam.Kuo <Sam.Kuo@microchip.com>
2022-08-15 23:01:53 +0800

    lua

--- gccCode/test.c
+++ gccCode/test.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 
+#define MAX_SIZE 6
+
+typedef struct stk{
+    char data[MAX_SIZE];
+    int front;
+    int rear;
+}stack;
+
+stack S;
+void enstack(stack *S,char value)
+{
+    /* S->data[++S->rear] = value; */
+    if ((S->rear +1)%MAX_SIZE == S->front)
+    {
+        /* full */
+    }
+    else
+    {
+        S->rear = (S->rear + 1)% MAX_SIZE;
+        S->data[S->rear] = value;
+    }
+}
+void destack(stack *S)
+{
+    if (S->front == S->rear)
+    {
+        /* empty */
+    }
+    else
+    {
+        S->front = (S->front + 1)%MAX_SIZE;
+        printf("%c",S->data[S->front]);
+    }
+}
 int main(void)
 {
-    int i;
+    S.front = 0;
+    S.rear  = 0;
+    /* int i; */
     /* FILE *fp; */
     /* fp = fopen("test.txt", "w+"); */
     /* fprintf(fp,"hexValue = ["); */

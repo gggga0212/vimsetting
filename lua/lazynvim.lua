57df3519e068cab553b4971c9fff2603da47ff11
Sam.Kuo <Sam.Kuo@microchip.com>
2023-06-28 14:19:19 +0800

    a

--- lua/lazynvim.lua
+++ lua/lazynvim.lua
@@ -200,19 +200,6 @@ require("lazy").setup({
         } },
     {'ibhagwan/fzf-lua'}, -- required for picker = "fzf-lua"
     -- {'nvim-tree/nvim-web-devicons'}, -- optional [for devicons in telesc ope or fzf]
-    -- load cscope maps
-    -- pass empty table to setup({}) for default options
-    -- require('cscope_maps').setup({
-    --   disable_maps = false, -- true disables my keymaps, only :Cscope will be loaded
-    --   skip_input_prompt = true, -- true doesn't ask for input
-    --   cscope = {
-    --     db_file = "./cscope.out", -- location of cscope db file
-    --     exec = "cscope", -- "cscope" or "gtags-cscope"
-    --     picker = "quickfix", -- "telescope", "fzf-lua" or "quickfix"
-    --     skip_picker_for_single_result = true, -- jump directly to position for single result
-    --     db_build_cmd_args = { "-bqkv" }, -- args used for db build (:Cscope build)
-    --   },
-    -- }),
      -- {'ldelossa/nvim-ide'},
     {'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
      config = function()
--- lua/options.lua
+++ lua/options.lua
@@ -19,6 +19,7 @@ require "lsp.nvim-cmp"
 require "lsp.null_ls"
 require "lsp.stuff"
 require "lsp.cscope"
+require "lsp.noice"
 
 -- :help option
 vim.opt.encoding      = 'utf-8'

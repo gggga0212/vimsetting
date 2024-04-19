53cf266902a34c88236344b6b951ef6728cf04f6
Sam.Kuo <Sam.Kuo@microchip.com>
2024-04-19 11:18:10 +0800

    a

--- lua/lazynvim.lua
+++ lua/lazynvim.lua
@@ -176,15 +176,15 @@ require("lazy").setup({
         -- USE EMPTY FOR DEFAULT OPTIONS
         -- DEFAULTS ARE LISTED BELOW
         } },
-    { "ludovicchabant/vim-gutentags",
-        after = "cscope_maps.nvim",
-        config = function()
-        vim.g.gutentags_modules = {"cscope_maps"} -- This is required. Other config is optional
-        vim.g.gutentags_cscope_build_inverted_index_maps = 1
-        vim.g.gutentags_cache_dir = vim.fn.expand("~/code/.gutentags")
-        vim.g.gutentags_file_list_command = "fd -e c -e h"
-        -- vim.g.gutentags_trace = 1
-    end, },
+    -- { "ludovicchabant/vim-gutentags",
+    --     after = "cscope_maps.nvim",
+    --     config = function()
+    --     vim.g.gutentags_modules = {"cscope_maps"} -- This is required. Other config is optional
+    --     vim.g.gutentags_cscope_build_inverted_index_maps = 1
+    --     vim.g.gutentags_cache_dir = vim.fn.expand("~/code/.gutentags")
+    --     vim.g.gutentags_file_list_command = "fd -e c -e h"
+    --     -- vim.g.gutentags_trace = 1
+    -- end, },
     -- {'mfussenegger/nvim-dap'},
     { 'mcauley-penney/visual-whitespace.nvim',
        config = true

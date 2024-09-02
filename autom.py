faf57a6531ecf314a3ac4656e22087405247a21c
Sam.Kuo <Sam.Kuo@microchip.com>
2024-09-02 16:43:31 +0800

    a
    a

--- autom.py
+++ autom.py
@@ -1,5 +1,9 @@
 import os
 import subprocess
+from colorama import init, Fore, Style
+
+# Initialize colorama
+init(autoreset=True)
 
 nv = "~/.config/nvim-linux64/bin/nvim"
 AutoPath = "AutoPTS_V3.0.7"
@@ -20,24 +24,34 @@ item_Auto = [
     "BAS_For_TCRL_2024_1.pts",
     "DIS_For_TCRL_2024_1.pts",
     "HIDS_For_TCRL_2024_1.pts",
-    "HOGPS_For_TCRL_2024_1.pts"
+    "HOGPS_For_TCRL_2024_1.pts",
+    "Back to the previous page"
 ]
 item_RunAutoPTS = [
     "cmd.exe /c python AutoPTS.py",
     "cmd.exe /c RunAutoPTS.bat",
-    "Run MiddleWareTest"
+    "Run MiddleWareTest",
+    "Back to the previous page"
 ]
 item_vimEdit = [
     "TestSet.json",
     "CaseDefine.json",
-    "AutoPTS.py"
+    "AutoPTS.py",
+    "Back to the previous page"
 ]
 item_Folder = [
     "ProfilesConfigs",
     "WorkDirectory",
-    "MiddleTestLog"
+    "MiddleTestLog",
+    "Back to the previous page"
 ]
 item_App = {
+    "Core": [
+        "Core_v5.2.pdf",
+        "Core_v5.3.pdf",
+        "Core_v5.4.pdf",
+        "Back to the previous page"
+    ],
     "ICS": [
         "GAP.ICS.p42.pdf  (2024.09.04)",
         "GATT.ICS.p19.pdf (2024.09.04)",
@@ -52,12 +66,10 @@ item_App = {
         "BAS.ICS.p5.pdf  (2024.07.01)",
         "DIS.ICS.p5.pdf  (2023.06.23)",
         "HIDS.ICS.p3.pdf  (2023.02.27)",
-        "HOGP.ICS.p8.pdf  (2024.07.01)"
+        "HOGP.ICS.p8.pdf  (2024.07.01)",
+        "Back to the previous page"
     ],
     "SPEC": [
-        "Core_v5.2.pdf",
-        "Core_v5.3.pdf",
-        "Core_v5.4.pdf"
         "ANP_SPEC_V10.pdf  (2011.09.05)",
         "ANS_SPEC_V10.pdf  (2011.09.15)",
         "PXP_v1.0.1.pdf    (2015.07.14)",
@@ -67,13 +79,14 @@ item_App = {
         "BAS_SPEC_V10.pdf  (2011.12.27)",
         "DIS_SPEC_V11r00.pdf  (2011.11.29)",
         "HIDS_SPEC_V10.pdf (2011.12.27)",
-        "HOGP_SPEC_V10.pdf (2011.12.27)"
+        "HOGP_SPEC_V10.pdf (2011.12.27)",
+        "Back to the previous page"
     ],
     "TS": [
         "GAP.TS.p46.pdf   (2024.09.04)",
         "GATT.TS.p26.pdf  (2024.09.04)",
         "L2CAP.TS.p38.pdf (2024.09.04)",
-        "SM.TS.p26.pdf    (2024.09.04)"
+        "SM.TS.p26.pdf    (2024.09.04)",
         "ANP.TS.1.0.3ed2.pdf (2019.11.11)",
         "ANS.TS.1.0.5ed2.pdf (2019.11.11)",
         "PXP.TS.p10.pdf  (2024.07.01)",
@@ -83,9 +96,12 @@ item_App = {
         "BAS.TS.p6.pdf  (2023.06.29)",
         "DIS.TS.p6.pdf  (2023.06.23)",
         "HIDS.TS.p6.pdf (2024.07.01)",
-        "HOGP.TS.p11.pdf (2024.07.01)"
+        "HOGP.TS.p11.pdf (2024.07.01)",
+        "Back to the previous page"
     ],
-    "Docs for SQA": [
+    "To_SQA": [
+        "BLE_Test_Command_Set.docx",
+        "Profile_Test_Command_Set.docx",
         "PTS_ANP_For_SQA.docx",
         "PTS_ANS_For_SQA.docx",
         "PTS_PXP_For_SQA.docx",
@@ -95,14 +111,14 @@ item_App = {
         "PTS_BAS_For_SQA.docx",
         "PTS_DIS_For_SQA.docx",
         "PTS_HIDS_For_SQA.docx",
-        "PTS_HOGPS_For_SQA.docx"
+        "PTS_HOGPS_For_SQA.docx",
+        "Back to the previous page"
     ],
-    "Others": [
-        "stack_test_script.ptp"
-        "BLE_Test_Command_Set.docx",
-        "Profile_Test_Command_Set.docx"
+    "Misc": [
+        "stack_test_script.ptp",
         "Assigned_Numbers.pdf (2023.07.25)",
-        "CSS_v11.pdf (2023.01.31)"
+        "CSS_v11.pdf (2023.01.31)",
+        "Back to the previous page"
     ]
 }
 
@@ -118,84 +134,155 @@ testSets = [
     "TCRL_2024_1_HOGP.json"
 ]
 
-def display_menu(items):
+def display_menu(items, title="Menu"):
+    print(Fore.CYAN + Style.BRIGHT + f"\n{title}\n" + "="*len(title))
     for idx, item in enumerate(items):
-        print(f"{idx + 1}. {item}")
-    choice = input("Select item please: ")
+        print(Fore.GREEN + f"{idx + 1}. {item}")
+    choice = input(Fore.YELLOW + "Select item please: ")
+    if choice.lower() == 'b':
+        return len(items) - 1
     try:
         choice = int(choice)
         if 1 <= choice <= len(items):
             return choice - 1
         else:
-            print("ByeBye")
+            print(Fore.RED + "Invalid choice. ByeBye")
             exit()
     except ValueError:
-        print("ByeBye")
+        print(Fore.RED + "Invalid input. ByeBye")
         exit()
 
 def selAuto():
-    SEL = display_menu(item_Auto)
-    os.remove(f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}/pics.pts")
-    subprocess.run([
-        "cp",
-        f"/mnt/c/mchpCode/AutoPTS/{item_Auto[SEL]}",
-        f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}/pics.pts"
-    ])
-    os.chdir(f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}/Function")
-    subprocess.run(["cmd.exe", "/c", "python", "GeneratePtsConfig.py", "../pics.pts"])
-    subprocess.run([
-        "cp",
-        f"/mnt/c/mchpCode/AutoPTS/{testSets[SEL]}",
-        f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}/TestSet.json"
-    ])
+    while True:
+        print(Fore.MAGENTA + Style.BRIGHT + """
+        █████╗ ██╗   ██╗████████╗ ██████╗ ███████╗
+       ██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗██╔════╝
+       ███████║██║   ██║   ██║   ██║   ██║███████╗
+       ██╔══██║██║   ██║   ██║   ██║   ██║╚════██║
+       ██║  ██║╚██████╔╝   ██║   ╚██████╔╝███████║
+       ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝
+        """)
+        SEL = display_menu(item_Auto, "Select AutoPTS Item")
+        if SEL == len(item_Auto) - 1:
+            break
+        os.remove(f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}/pics.pts")
+        subprocess.run([
+            "cp",
+            f"/mnt/c/mchpCode/AutoPTS/{item_Auto[SEL]}",
+            f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}/pics.pts"
+        ])
+        os.chdir(f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}/Function")
+        subprocess.run(["cmd.exe", "/c", "python", "GeneratePtsConfig.py", "../pics.pts"])
+        subprocess.run([
+            "cp",
+            f"/mnt/c/mchpCode/AutoPTS/{testSets[SEL]}",
+            f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}/TestSet.json"
+        ])
 
 def selRunAutoPTS():
-    SEL = display_menu(item_RunAutoPTS)
-    if SEL <= 1:
-        os.chdir(f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}")
-        subprocess.run(item_RunAutoPTS[SEL], shell=True)
-    elif SEL == 2:
-        os.chdir(f"/mnt/c/mchpCode/AutoPTS/{MidPath}/{MidPath}")
-        subprocess.run(["cmd.exe", "/c", "python", "main.py"])
+    while True:
+        print(Fore.MAGENTA + Style.BRIGHT + """
+        ██████╗ ██╗   ██╗███╗   ██╗ █████╗     ██████╗ ████████╗███████╗
+       ██╔═══██╗██║   ██║████╗  ██║██╔══██╗    ██╔══██╗╚══██╔══╝██╔════╝
+       ██║   ██║██║   ██║██╔██╗ ██║███████║    ██████╔╝   ██║   █████╗  
+       ██║   ██║██║   ██║██║╚██╗██║██╔══██║    ██╔═══╝    ██║   ██╔══╝  
+       ╚██████╔╝╚██████╔╝██║ ╚████║██║  ██║    ██║        ██║   ███████╗
+        ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝    ╚═╝        ╚═╝   ╚══════╝
+        """)
+        SEL = display_menu(item_RunAutoPTS, "Select RunAutoPTS Item")
+        if SEL == len(item_RunAutoPTS) - 1:
+            break
+        if SEL <= 1:
+            os.chdir(f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}")
+            subprocess.run(item_RunAutoPTS[SEL], shell=True)
+        elif SEL == 2:
+            os.chdir(f"/mnt/c/mchpCode/AutoPTS/{MidPath}/{MidPath}")
+            subprocess.run(["cmd.exe", "/c", "python", "main.py"])
 
 def selvimEdit():
-    SEL = display_menu(item_vimEdit)
-    os.chdir(f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}")
-    cmd = f"{nv} {item_vimEdit[SEL]}"
-    os.system(cmd)
+    while True:
+        print(Fore.MAGENTA + Style.BRIGHT + """
+        ██╗   ██╗██╗███╗   ███╗███████╗███████╗████████╗
+        ██║   ██║██║████╗ ████║██╔════╝██╔════╝╚══██╔══╝
+        ██║   ██║██║██╔████╔██║█████╗  ███████╗   ██║   
+        ██║   ██║██║██║╚██╔╝██║██╔══╝  ╚════██║   ██║   
+        ╚██████╔╝██║██║ ╚═╝ ██║███████╗███████║   ██║   
+         ╚═════╝ ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝   ╚═╝   
+        """)
+        SEL = display_menu(item_vimEdit, "Select vimEdit Item")
+        if SEL == len(item_vimEdit) - 1:
+            break
+        os.chdir(f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}")
+        cmd = f"{nv} {item_vimEdit[SEL]}"
+        os.system(cmd)
 
 def selFolder():
-    SEL = display_menu(item_Folder)
-    os.chdir(f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}")
-    os.system(f'{nv} .')
+    while True:
+        print(Fore.MAGENTA + Style.BRIGHT + """
+        ███████╗ ██████╗ ██████╗ ██╗     ███████╗██████╗ 
+        ██╔════╝██╔════╝██╔═══██╗██║     ██╔════╝██╔══██╗
+        ███████╗██║     ██║   ██║██║     █████╗  ██████╔╝
+        ╚════██║██║     ██║   ██║██║     ██╔══╝  ██╔══██╗
+        ███████║╚██████╗╚██████╔╝███████╗███████╗██║  ██║
+        ╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝
+        """)
+        SEL = display_menu(item_Folder, "Select Folder Item")
+        if SEL == len(item_Folder) - 1:
+            break
+        os.chdir(f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}")
+        os.system(f'{nv} .')
 
 def selApp():
-    categories = list(item_App.keys())
-    category_choice = display_menu(categories)
-    selected_category = categories[category_choice]
-    items = item_App[selected_category]
+    while True:
+        print(Fore.MAGENTA + Style.BRIGHT + """
+        █████╗ ██████╗ ██████╗ 
+       ██╔══██╗██╔══██╗██╔══██╗
+       ███████║██████╔╝██████╔╝
+       ██╔══██║██╔═══╝ ██╔═══╝ 
+       ██║  ██║██║     ██║     
+       ╚═╝  ╚═╝╚═╝     ╚═╝     
+        """)
+        categories = list(item_App.keys()) + ["Back to the previous page"]
+        category_choice = display_menu(categories, "Select App Category")
+        if category_choice == len(categories) - 1:
+            break
+        selected_category = categories[category_choice]
+        items = item_App[selected_category]
 
-    SEL = display_menu(items)
-    os.chdir("/mnt/c/mchpCode")
-    subprocess.run(["cmd.exe", "/c", "start", items[SEL]])
+        while True:
+            SEL = display_menu(items, f"Select {selected_category} Item")
+            if SEL == len(items) - 1:
+                break
+            os.chdir("/mnt/c/mchpCode")
+            subprocess.run(["cmd.exe", "/c", "start", items[SEL]])
 
 def mainInit():
     os.chdir(f"/mnt/c/mchpCode/AutoPTS/{AutoPath}/{AutoPath}")
     if os.path.isfile("TestSet_old.json"):
         os.remove("TestSet_old.json")
 
-    SEL = display_menu(index)
-    print(f"Selected item #{SEL + 1} which means {index[SEL]}")
-    if SEL == 0:
-        selAuto()
-    elif SEL == 1:
-        selRunAutoPTS()
-    elif SEL == 2:
-        selvimEdit()
-    elif SEL == 3:
-        selFolder()
-    elif SEL == 4:
-        selApp()
+    print(Fore.MAGENTA + Style.BRIGHT + """
+    ███╗   ███╗ █████╗ ██╗███╗   ██╗    ███╗   ███╗███████╗███╗   ██╗██╗   ██╗
+    ████╗ ████║██╔══██╗██║████╗  ██║    ████╗ ████║██╔════╝████╗  ██║██║   ██║
+    ██╔████╔██║███████║██║██╔██╗ ██║    ██╔████╔██║█████╗  ██╔██╗ ██║██║   ██║
+    ██║╚██╔╝██║██╔══██║██║██║╚██╗██║    ██║╚██╔╝██║██╔══╝  ██║╚██╗██║██║   ██║
+    ██║ ╚═╝ ██║██║  ██║██║██║ ╚████║    ██║ ╚═╝ ██║███████╗██║ ╚████║╚██████╔╝
+    ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝ 
+    """)
+
+    while True:
+        SEL = display_menu(index, "Main Menu")
+        print(Fore.CYAN + f"Selected item #{SEL + 1} which means {index[SEL]}")
+        if SEL == 0:
+            selAuto()
+        elif SEL == 1:
+            selRunAutoPTS()
+        elif SEL == 2:
+            selvimEdit()
+        elif SEL == 3:
+            selFolder()
+        elif SEL == 4:
+            selApp()
 
 if __name__ == "__main__":
     mainInit()

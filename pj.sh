4550b6c06eed0ba77d91146d90d1fb0a1adddc39
Sam.Kuo <Sam.Kuo@microchip.com>
2024-09-21 16:08:40 +0800

    a

--- pj.sh
+++ pj.sh
@@ -3,10 +3,3 @@ echo "${date}"
 set 'date'
 echo $(4)
 
-# projectName = 
-# git clone --recursive https://bitbucket.microchip.com/projects/WSGSW/repos/pic32cx-bz_ble_internal_apps.git 
-# cd pjname
-# git cotest
-# gototeam
-# git coteam
-# cd -

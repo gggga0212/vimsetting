ce95d3f656a98c04bf764086735045d11eb1ad79
Sam.Kuo <Sam.Kuo@microchip.com>
2024-10-16 20:13:00 +0800

    a

--- install_2.sh
+++ install_2.sh
@@ -3,5 +3,5 @@
 cd ~/.config
 rm -rf nvim-linux64.tar.gz
 rm -rf nvim-linux64.tar
-wget https://github.com/neovim/neovim/releases/download/v0.10.1/nvim-linux64.tar.gz
+wget https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz

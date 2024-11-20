f97a6ff25768da1d8b40d5d37b3067bd7906117f
Sam.Kuo <Sam.Kuo@microchip.com>
2024-11-20 14:24:10 +0800

    a

--- install_1.sh
+++ install_1.sh
@@ -2,7 +2,7 @@
 # Diffview needs the lastest git version
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt update
-sudo apt install -y git
+sudo apt install -y wget git
 sudo apt-get install -y curl
 
 git clone https://github.com/gggga0212/vimsetting ~/.config/nvim

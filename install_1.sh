8b5bbfdb7e1c495cb9cd2c2d05daaab70d65a2c6
Sam.Kuo <Sam.Kuo@microchip.com>
2025-01-10 09:37:33 +0800

    a

--- install_1.sh
+++ install_1.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt update
 sudo apt install -y wget git
-sudo apt-get install -y curl zsh
+sudo apt-get install -y curl
 
 git clone https://github.com/gggga0212/vimsetting ~/.config/nvim
 cd ~/.config/nvim

d8c11bddc85a108505245b07a22d6ceb6e923d24
samkuo <samkuo@Jmicron.com>
2025-12-29 23:57:50 +0800

    a

--- install_1.sh
+++ install_1.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository ppa:git-core/ppa
 sudo add-apt-repository ppa:ubuntu-support-team/gdb
 sudo apt update
 sudo apt install -y wget git gdb
-sudo apt-get install -y zsh curl uzip npm
+sudo apt-get install -y zsh curl unzip npm
 
 git clone https://github.com/gggga0212/vimsetting ~/.config/nvim
 cd ~/.config/nvim

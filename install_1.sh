4b954cb5b07cd4fb243ed9d4567ccc8a26a3eb00
sam.kuo <sam.kuo@gearradio.net>
2025-12-29 20:06:28 +0800

    a

--- install_1.sh
+++ install_1.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Diffview needs the lastest git version
-wsl --set-default-version 1
 sudo add-apt-repository ppa:git-core/ppa
 sudo add-apt-repository ppa:ubuntu-support-team/gdb
 sudo apt update
 sudo apt install -y wget git gdb
-sudo apt-get install -y zsh curl
+sudo apt-get install -y zsh curl uzip npm
 
 git clone https://github.com/gggga0212/vimsetting ~/.config/nvim
 cd ~/.config/nvim

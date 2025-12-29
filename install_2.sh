7048d42f2ab49b2906deffdf1a184a9e7619df46
sam.kuo <sam.kuo@gearradio.net>
2025-12-29 21:09:36 +0800

    a

--- install_2.sh
+++ install_2.sh
@@ -11,8 +11,15 @@ NVIM_ARCHIVE="nvim-linux-x86_64"
 NVIM_VERSION="v0.11.4"
 DOWNLOAD_URL="https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/${NVIM_ARCHIVE}.tar.gz"
 
+LZ_VERSION="0.57.0"
+DOWNLOAD_LZURL="https://github.com/jesseduffield/lazygit/releases/download/v${LZ_VERSION}/lazygit_${LZ_VERSION}_windows_x86_64.zip"
+
 rm -rf "${NVIM_ARCHIVE}.tar.gz" "${NVIM_ARCHIVE}.tar" "$NVIM_ARCHIVE"
+
+rm -rf "lazygit_${LZ_VERSION}_windows_x86_64.zip" 
+
 wget "$DOWNLOAD_URL"
-wget "https://github.com/jesseduffield/lazygit/releases/download/v0.55.1/lazygit_0.55.1_windows_x86_64.zip"
+wget "https://github.com/jesseduffield/lazygit/releases/download/v${LZ_VERSION}/lazygit_${LZ_VERSION}_windows_x86_64.zip"
 tar xzvf "${NVIM_ARCHIVE}.tar.gz"
-unzip lazygit_0.55.1_windows_x86_64.zip -d lazygit
+unzip lazygit_${LZ_VERSION}_windows_x86_64.zip -d lazygit
+rm -rf lazygit_${LZ_VERSION}_windows_x86_64.zip

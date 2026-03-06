85097f20ce8e5f1474c92ceecb6dc2e0a09d46de
samkuo <samkuo@jmicron.com>
2026-03-06 16:47:08 +0800

    a

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -27,10 +27,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'gk', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>', opts)
+    vim.keymap.set('n', 'gd', ':Telescope lsp_definitions<CR>', opts)
+    vim.keymap.set('n', 'gD', ':Telescope lsp_declarations<CR>', opts)
+    vim.keymap.set('n', 'gi', ':Telescope lsp_implementations<CR>', opts)
+
     -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
@@ -40,7 +42,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<space>f', function()
       vim.lsp.buf.format { async = true }
     end, opts)
